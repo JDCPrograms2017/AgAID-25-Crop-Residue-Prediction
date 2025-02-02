66c4b912789b2eb3699db49d8092c5ac2d8d96ed
JDCPrograms2017 <jchadwick423@gmail.com>
2025-02-02 12:54:13 -0800

    Worked on test.py

--- model/test.py
+++ model/test.py
@@ -2,6 +2,7 @@
 # images
 import torch 
 import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
 from torchvision.models.segmentation import deeplabv3_resnet101
 from PIL import Image
 import matplotlib.pyplot as plt
@@ -10,6 +11,7 @@ from utils import CropResidueSegDataset
 
 if __name__ == "__main__":
     test1 = CropResidueSegDataset("../test", False)
+    dataset_loader = DataLoader(test1, batch_size=16, shuffle=True, num_workers=4)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     model = deeplabv3_resnet101(weights=None)
@@ -18,4 +20,10 @@ if __name__ == "__main__":
     model.load_state_dict(torch.load("../agaid2025_final.pth"))
     model.to(device)
     model.eval()
+        
+    for images, _ in dataset_loader:
+        images = images.to(device)
+
+        with torch.no_grad():
+            outputs = model(images)["out"] # Get the output Tensor!
 
